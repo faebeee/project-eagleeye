2df1aadea28aff431fd75fcc335d97216d6b2514
Fabio Gianini <faebeee@gmail.com>
2023-09-08 22:36:25 +0200

    Fix types

--- lib/css-spec/css-spec.ts
+++ lib/css-spec/css-spec.ts
@@ -3,7 +3,7 @@ import cssstats from 'cssstats'
 
 export const cssSpec = async (site: string) => {
   const css = await getCss(site);
-  const singleCssContent = css.links.map((link) => link.css).join(' ');
+  const singleCssContent = css.links.map((link:{css:string}) => link.css).join(' ');
   const stats = cssstats(singleCssContent)
   return stats
 
