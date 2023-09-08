dc4175c61c9597260d5022552256d33a9c61645f
Fabio Gianini <faebeee@gmail.com>
2023-09-08 22:35:49 +0200

    Cleanup

--- lib/css-spec/css-spec.ts
+++ lib/css-spec/css-spec.ts
@@ -2,7 +2,6 @@ import getCss from 'get-css'
 import cssstats from 'cssstats'
 
 export const cssSpec = async (site: string) => {
-  console.log(site);
   const css = await getCss(site);
   const singleCssContent = css.links.map((link) => link.css).join(' ');
   const stats = cssstats(singleCssContent)
